99b32535ec340bbe90171a7ffb9a2f54c90183f9
SeidorPortalsUSA <seidorportals@gmail.com>
2023-10-01 19:41:02 -0600

    delete

--- basico2.py
+++
@@ -1,9 +0,0 @@
-#Ejercicio 2 - Lectura de Archivos. Crea un programa que lea un archivo de texto llamado "datos.txt"
-#y muestre su contenido en pantalla.
-
-try:
-    with open("datos.txt", "r") as archivo:
-        contenido = archivo.read()
-        print(contenido)
-except FileNotFoundError:
-    print("El archivo 'datos.txt' no se encontró.")
--- basico4.py
+++
@@ -1,8 +0,0 @@
-# Ejercicio 4 - Escritura de Archivos. Crea un programa que escriba una lista de nombres en un
-# archivo llamado "nombres.txt", un nombre por línea.
-
-nombres = ["Diana", "Melvin", "Alyssa", "Federico"]
-
-with open("nombres.txt", "w") as archivo:
-    for nombre in nombres:
-        archivo.write(nombre + "\n")
